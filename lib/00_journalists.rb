de42f62b4f942dc83a8ac211cd6b3e62b036457e
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-06 18:31:54 +0200

    Data Scientist Exercices

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,20 +1,23 @@
-
+#_______________________twitterHandle___________________________
 def twitterHandle 
     ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 end 
 
+#_________________________request1______________________________
 def request1(twitter)
-    puts "Nbre de Handle : #{twitter.length}"
+    puts "\n Nbre de Handle : #{twitter.length}\n"
 end
 
+#_________________________request2______________________________
 def request2(twitter)
     twitter.sort_by!(&:length)
-    puts "Handle le plus court : #{twitter[0]}"
+    puts "\n Handle le plus court : #{twitter[0]}\n"
 end
 
+#_________________________request3______________________________
 def request3(twitter)
-    # création du tableau  "stringLength"  contenant la taille de chaque Handle (-1 pour éliminer le caractère "@")
-    stringLength = twitter.map do |x| 
+                                         # création du tableau  "stringLength"
+    stringLength = twitter.map do |x|    # contenant la taille de chaque Handle (-1 pour éliminer le caractère "@")
         x.length-1
     end
     y = 0                                # initialisiation de y à 0
@@ -23,78 +26,98 @@ def request3(twitter)
             y += 1                       # j'incrémente y
         end
     end
-    puts "Nbre de Handle contenants 5 caractères : #{y}"
+    puts "\n Nbre de Handle contenants 5 caractères : #{y}\n"
 end
 
+#__________________________request4_____________________________
 def request4(twitter)
-                 # grep va filtrer les handle qui contiennent @ + un Majuscule et count va les compter
-    puts "Nbre de Handle commençants par une Majuscule :  #{twitter.grep(/@[A-Z]/).count}"
+                                    # grep va filtrer les handle qui contiennent @ + un Majuscule et count va les compter
+    puts "\n Nbre de Handle commençants par une Majuscule :  #{twitter.grep(/@[A-Z]/).count}\n"
 end
 
+#__________________________request5_____________________________
 def request5(twitter)
-    puts "Liste triée par ordre Alphabétique : "
-    puts
+    puts "\n Liste triée par ordre Alphabétique : \n"
     tri = twitter.map(&:downcase)
-    print tri.sort #sort va trier par ordre alphabétique croissant
+    print tri.sort                  # sort va trier par ordre alphabétique croissant
     puts
 end
 
+#__________________________request6_____________________________
 def request6(twitter)
-    puts "Liste triée par taille croissante des Handle : "
-    puts
-    puts "#{twitter.sort_by(&:length)}"
+    puts "\n Liste triée par taille croissante des Handle : "
+    puts "#{twitter.sort_by(&:length)}\n"
 end
 
+#__________________________request7_____________________________
 def request7(twitter)
-    puts "Position dans l'array de la personne @epenser : #{twitter.index "@epenser"}"
+    puts "\n Position dans l'array de la personne @epenser : #{twitter.index "@epenser"}\n"
 end
 
+#__________________________request8_____________________________
 def request8(twitter)
-    twitter.sort_by!(&:length) #tri du tableau par longueur de chaine de caractere
-
+    twitter.sort_by!(&:length)      # tri du tableau par longueur de chaine de caractere
     twitter2 = twitter.map do |n| 
-        n.length        # on crée un tableau contenant la valeur des longueurs
+        n.length                    # on crée un tableau contenant la valeur des longueurs
     end
-
     compteur = 0
     refBase = twitter2[0]
     x = twitter2.length
-
     for i in 0...x
-            if refBase != twitter2[i]
-                puts "Handle size = #{refBase} total = #{compteur}"
-                refBase = twitter2[i] # réinitialise le taille de référence
-                compteur = 0   # on réinitialise le compteur
-            end 
-            compteur += 1
+        if refBase != twitter2[i]
+            puts " Handle size = #{refBase} total = #{compteur}"
+            refBase = twitter2[i]   # réinitialise le taille de référence
+            compteur = 0            # on réinitialise le compteur
+        end 
+        compteur += 1
     end
-    puts "Handle size = #{refBase} total = #{compteur}"
-    puts
-    puts
+    puts " Handle size = #{refBase} total = #{compteur}\n"
 end
 
+#___________________________separe________________________________
 def separe
-    puts
-    print "x"*80
-    puts
+    print "-"*80
     puts
 end
 
-separe
-request1(twitterHandle)
-separe
-request2(twitterHandle)
-separe
-request3(twitterHandle)
-separe
-request4(twitterHandle)
-separe
-request5(twitterHandle)
-separe
-request6(twitterHandle)
-separe
-request7(twitterHandle)
-separe
-request8(twitterHandle)
+#____________________________menu_________________________________
+def menu 
+    exo = nil
+    while exo != 0
+        print "\n _ Quel exercice voulez-vous exécuter ? [ 1 ... 8 ] : "
+        exo = gets.to_i
+        separe
+        if exo == 1
+            request1(twitterHandle)
+            end
+        if exo == 2
+            request2(twitterHandle)
+            end
+        if exo == 3
+            request3(twitterHandle)
+            end
+        if exo == 4
+            request4(twitterHandle)
+            end
+        if exo == 5
+            request5(twitterHandle)
+                end
+        if exo == 6
+            request6(twitterHandle)
+                end
+        if exo == 7
+            request7(twitterHandle)
+                end
+        if exo == 8
+            request8(twitterHandle)
+                end
+        if exo >= 9
+                exo = 0
+            end
+    end
+end 
+#_____________________________________________________________
+
+menu
 
 
